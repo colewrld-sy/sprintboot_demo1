26c7be26c545cc04a0eedf51e87f862313565a75
lrc <1922512531@qq.com>
2025-03-05 17:21:43 +0800

    1

--- demo1/src/main/java/com/example/demo1/controller/UserController.java
+++ demo1/src/main/java/com/example/demo1/controller/UserController.java
@@ -18,7 +18,7 @@ public class UserController {
     //增加
     @PostMapping
     //传入json文本自动转对象
-    public String add(@RequestBody UserDto user){
+    public ResponseMessage add(@RequestBody UserDto user){
         User userNew=userService.add(user);
         return ResponseMessage.success(userNew);
     }
